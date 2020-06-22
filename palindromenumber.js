1df6318cdc59bebb4db319687f055a416f4f7c79
kevindai777 <kevin.dai.2451@gmail.com>
2020-06-22 03:41:57 -0400

    solutions

---
+++ palindromenumber.js
@@ -0,0 +1,19 @@
+//Objective is to see whether a number is a palindrome. '121' is an example.
+//Negative numbers cannot be palindromes.
+
+let number = 121
+
+
+//O(logn) solution that divides the number by 10 and adds the remainder to a
+//new temp value. It checks whether the temp and original are the same.
+
+let result = 0
+let temp = number
+
+while (temp > 0) {
+    //Shift the digits and add the remainder
+    result = (result * 10) + (temp % 10)
+    temp = Math.floor(temp / 10)
+}
+
+number == result
